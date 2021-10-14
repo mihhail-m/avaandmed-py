20e24badfb71c886a6017f468a9cc0a8c629f038
mihhail <mihhail.matisinets@gmail.com>
2021-10-14 16:06:24 +0300

    Created workable version of http client and added some tests

--- avaandmed/http/http_client.py
+++ avaandmed/http/http_client.py
@@ -1,8 +1,8 @@
 import requests
-from typing import Dict
+from base64 import b64encode
 from enum import Enum
 
-from avaandmed.entities.api_resource import ApiResource
+from avaandmed.exceptions import AvaandmedApiExcepiton
 
 
 class HttpMethod(Enum):
@@ -12,21 +12,58 @@ class HttpMethod(Enum):
     UPDATE = 'update'
 
 
+class AllowedLang(Enum):
+    EN = 'en'
+    ET = 'en'
+
+
 class HttpClient:
-    __headers: Dict = {
+    """
+    Class that is responsible for basic HTTP logic for the client.
+    """
+    __headers = {
         'content-Type': 'application/json'
     }
+    __SCHEME = 'https'
+    __HOSTNAME = 'avaandmed.eesti.ee'
+    __BASE_ENDPOINT = 'api'
+    __BASE_URL = f"{__SCHEME}://{__HOSTNAME}/{__BASE_ENDPOINT}"
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, api_key: str = None, key_id: str = None) -> None:
+        self.__api_key = api_key
+        self.__key_id = key_id
 
-    def get_token(self) -> str:
-        pass
+    def __get_token(self) -> str:
+        """
+        Makes a requst to /auth/key-login endpoint and retrieves an access token
+        to authorize future requests.
+        """
+        def encode_key():
+            key = f"{self.__key_id}:{self.__api_key}".encode('ascii')
+            return b64encode(key).decode('ascii')
 
-    def request(self, method: HttpMethod, url: str, data: ApiResource):
-        params = '' if data is None else data.json()
+        x_api_key = encode_key()
+        self.__headers['X-API-KEY'] = x_api_key
+        auth_url = f"{self.__BASE_URL}/auth/key-login"
+        r = requests.post(auth_url, headers=self.__headers)
+
+        if r.status_code != 201:
+            msg = r.json()['message']
+            raise AvaandmedApiExcepiton(
+                status=r.status_code, uri=r.url, msg=msg)
+
+        return r.json()['data']['accessToken']
+
+    def request(self, method: HttpMethod, url: str, data={}, lang: AllowedLang = AllowedLang.EN):
+        """
+        Generic request method to make request to the API.
+        """
+        url = f"{self.__BASE_URL}{url}?lang={lang.value}"
+        access_token = self.__get_token()
+        self.__headers['Authorization'] = f"Bearer {access_token}"
         res = requests.request(method=method.name,
                                url=url,
-                               json=params,
+                               json=data,
                                headers=self.__headers)
-        return res.json()
+
+        return res.json()['data']
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,19 +1,21 @@
-import json
+import os
 from avaandmed.entities.organizations import Organizations
-from avaandmed.entities.datasets import Datasets
+from avaandmed.entities.datasets import Datasets, Dataset
 from avaandmed import Avaandmed
-from avaandmed.http.http_client import HttpMethod
+from avaandmed.http.http_client import HttpClient, HttpMethod
 
-API_TOKEN = "token"
-KEY_ID = "id"
+API_TOKEN = os.getenv('AVAANDMED_KEY', 'none')
+KEY_ID = os.getenv('AVAANDMED_KEY_ID', 'none')
 
 client = Avaandmed(API_TOKEN, KEY_ID)
+DATASET_ID = '8d681e55-4118-41f5-b319-1d2bdd36408c'
+DATASET_SLUG = 'soidukite-staatused-eestis'
 
 
 def test_client_init():
+    assert API_TOKEN != 'none'
+    assert KEY_ID != 'none'
     assert isinstance(client, Avaandmed)
-    assert client.api_token == "token"
-    assert client.key_id == "id"
 
 
 def test_datasets_entity():
@@ -35,7 +37,18 @@ def test_http_methods_enum():
 
 def test_json_to_model():
     datasets_json_path = "C:\\Users\\mihha\\Desktop\\Projects\\bcs\\avaandmed-python\\tests\\data\\dataset.json"
-    dataset = Datasets.parse_file(datasets_json_path)
+    dataset = Dataset.parse_file(datasets_json_path)
     # print(dataset.name)
     assert dataset.name == 'dataset1'
-    assert isinstance(dataset, Datasets)
+    assert isinstance(dataset, Dataset)
+
+
+def test_dataset_retrieve_by_id():
+    dataset = client.datasets.retrieve_by_id(DATASET_ID)
+    assert DATASET_ID == dataset.id
+
+
+def test_dataset_retrieve_by_slug():
+    dataset = client.datasets.retrieve_by_slug(DATASET_SLUG)
+    # print(dataset)
+    assert DATASET_SLUG == dataset.slug
