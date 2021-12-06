bb874115870aeb671c2593756783b4ce29be2b9f
mihhail <mihhail.matisinets@gmail.com>
2021-12-06 13:59:25 +0200

    Added new tests. Upgraded old

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,4 +1,6 @@
 # type: ignore
+from os import access
+from attr import validate
 import pytest
 import responses
 from pathlib import Path
@@ -31,6 +33,7 @@ from .utils import load_json
 DATA_DIR = Path('data')
 DATASET_ID = '8d681e55-4118-41f5-b319-1d2bdd36408c'
 DATASET_ID_2 = '4446e091-adfc-4e1a-9e13-733d2b95f6e4'
+PROTO_DATASET_ID = 'c114a8a9-40ed-46c9-824f-dda1d92776de'
 DATASET_SLUG = 'soidukite-staatused-eestis'
 FILE_ID = 'b88c9edc-cf81-47a5-aaf0-40d2af2c73a1'
 WRONG_SLUG = 'sdfsdf'
@@ -103,11 +106,11 @@ def test_negative_post_auth(datasets: Datasets):
     with pytest.raises(AvaandmedApiExcepiton):
         mock_negative_post_auth(401, "Unauthorized")
         stub_get([DATASET_ID], MOCK_DATASET_FILE)
-        dataset = datasets.retrieve_by_id(DATASET_ID)
+        dataset = datasets.get_by_id(DATASET_ID)
 
 
 @responses.activate
-def test_retrieve_by_id(datasets: Datasets):
+def test_get_by_id(datasets: Datasets):
     mock_post_auth()
     responses.add(
         responses.GET,
@@ -116,14 +119,14 @@ def test_retrieve_by_id(datasets: Datasets):
         status=200
     )
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     assert isinstance(dataset, Dataset)
     assert DATASET_ID == dataset.id
     assert dataset.user is not None
 
 
 @responses.activate
-def test_negative_retrieve_by_id(datasets: Datasets):
+def test_negative_get_by_id(datasets: Datasets):
     with pytest.raises(AvaandmedApiExcepiton):
         mock_post_auth()
         responses.add(
@@ -133,11 +136,11 @@ def test_negative_retrieve_by_id(datasets: Datasets):
             status=404
         )
 
-        datasets.retrieve_by_id(WRONG_ID)
+        datasets.get_by_id(WRONG_ID)
 
 
 @responses.activate
-def test_retrieve_by_slug(datasets: Datasets):
+def test_get_by_slug(datasets: Datasets):
     mock_post_auth()
     responses.add(
         responses.GET,
@@ -146,13 +149,13 @@ def test_retrieve_by_slug(datasets: Datasets):
         status=200
     )
 
-    dataset = datasets.retrieve_by_slug(DATASET_SLUG)
+    dataset = datasets.get_by_slug(DATASET_SLUG)
     assert isinstance(dataset, Dataset)
     assert DATASET_SLUG == dataset.slug
 
 
 @responses.activate
-def test_negative_retrieve_by_slug(datasets: Datasets):
+def test_negative_get_by_slug(datasets: Datasets):
     with pytest.raises(AvaandmedApiExcepiton):
         mock_post_auth()
         responses.add(
@@ -162,7 +165,7 @@ def test_negative_retrieve_by_slug(datasets: Datasets):
             status=404
         )
 
-        datasets.retrieve_by_slug(WRONG_SLUG)
+        datasets.get_by_slug(WRONG_SLUG)
 
 
 @responses.activate
@@ -237,7 +240,7 @@ def test_user_is_deserialized(datasets: Datasets):
         status=200
     )
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     user_dict = dataset.user.dict()  # type: ignore
     for v in user_dict.values():
         assert v is not None
@@ -254,7 +257,7 @@ def test_organization_is_deserialized(datasets: Datasets):
         status=200
     )
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     organization_dict = dataset.organization.dict()
     assert isinstance(dataset.organization, Organization)
     for v in organization_dict.values():
@@ -265,7 +268,7 @@ def test_organization_is_deserialized(datasets: Datasets):
 def test_files_are_deserialized(datasets: Datasets):
     stub_get([DATASET_ID], MOCK_DATASET_FILE)
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     files = dataset.files
     assert isinstance(files[0], File)
 
@@ -274,7 +277,7 @@ def test_files_are_deserialized(datasets: Datasets):
 def test_keywords_are_deserialized(datasets: Datasets):
     stub_get([DATASET_ID], MOCK_DATASET_FILE)
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     keywords = dataset.keywords
     assert isinstance(keywords[0], Keyword)
 
@@ -283,7 +286,7 @@ def test_keywords_are_deserialized(datasets: Datasets):
 def test_categories_are_deserialized(datasets: Datasets):
     stub_get([DATASET_ID], MOCK_DATASET_FILE)
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     categories = dataset.categories
     assert isinstance(categories[0], Category)
 
@@ -292,7 +295,7 @@ def test_categories_are_deserialized(datasets: Datasets):
 def test_regions_are_deserialized(datasets: Datasets):
     stub_get([DATASET_ID], MOCK_DATASET_FILE)
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     regions = dataset.regions
     assert isinstance(regions[0], Region)
 
@@ -301,7 +304,7 @@ def test_regions_are_deserialized(datasets: Datasets):
 def test_coords_are_deserialized(datasets: Datasets):
     stub_get([DATASET_ID], MOCK_DATASET_FILE)
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     coords = dataset.coordinate_reference_systems
     assert isinstance(coords[0], CoordinateReferenceSystem)
 
@@ -310,7 +313,7 @@ def test_coords_are_deserialized(datasets: Datasets):
 def test_citations_are_deserialized(datasets: Datasets):
     stub_get([DATASET_ID], MOCK_DATASET_FILE)
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     citations = dataset.citations
     assert isinstance(citations[0], Citation)
 
@@ -319,7 +322,7 @@ def test_citations_are_deserialized(datasets: Datasets):
 def test_conformities_are_deserialized(datasets: Datasets):
     stub_get([DATASET_ID], MOCK_DATASET_FILE)
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     conformities = dataset.conformities
     assert isinstance(conformities[0], Conformity)
 
@@ -328,7 +331,7 @@ def test_conformities_are_deserialized(datasets: Datasets):
 def test_license_is_deserialized(datasets: Datasets):
     stub_get([DATASET_ID], MOCK_DATASET_FILE)
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     licence = dataset.licence
     assert isinstance(licence, Licence)
 
@@ -337,7 +340,7 @@ def test_license_is_deserialized(datasets: Datasets):
 def test_interval_is_deserialized(datasets: Datasets):
     stub_get([DATASET_ID], MOCK_DATASET_FILE)
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     interval = dataset.update_interval_unit
     assert isinstance(interval, UpdateIntervalUnit)
 
@@ -346,7 +349,7 @@ def test_interval_is_deserialized(datasets: Datasets):
 def test_access_is_deserialized(datasets: Datasets):
     stub_get([DATASET_ID], MOCK_DATASET_FILE)
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     access = dataset.access
     assert isinstance(access, Access)
 
@@ -355,7 +358,7 @@ def test_access_is_deserialized(datasets: Datasets):
 def test_res_type_is_deserialized(datasets: Datasets):
     stub_get([DATASET_ID], MOCK_DATASET_FILE)
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     res_type = dataset.resource_type
     assert isinstance(res_type, ResourceType)
 
@@ -364,7 +367,7 @@ def test_res_type_is_deserialized(datasets: Datasets):
 def test_topics_are_deserialized(datasets: Datasets):
     stub_get([DATASET_ID], MOCK_DATASET_FILE)
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     topics = dataset.topic_categories
 
     for topic in topics:
@@ -375,7 +378,7 @@ def test_topics_are_deserialized(datasets: Datasets):
 def test_notifications_are_deserialized(datasets: Datasets):
     stub_get([DATASET_ID], MOCK_DATASET_FILE)
 
-    dataset = datasets.retrieve_by_id(DATASET_ID)
+    dataset = datasets.get_by_id(DATASET_ID)
     notifs = dataset.organization.notifications
 
     for n in notifs:
@@ -395,7 +398,7 @@ def test_get_file_preview(datasets: Datasets):
 def test_get_dataset2(datasets: Datasets):
     mock_post_auth()
     stub_get([DATASET_ID_2], MOCK_DATSET_FILE_2)
-    dataset = datasets.retrieve_by_id(DATASET_ID_2)
+    dataset = datasets.get_by_id(DATASET_ID_2)
 
     assert dataset.parent_datasets is not None
 
@@ -416,15 +419,148 @@ def test_get_dataset_rating_by_slug(datasets: Datasets):
         responses.GET,
         join_base_url_values(['rating', DATASET_SLUG]),
         json={
-            "data": 0
+            "data": "5.00"
         },
         status=200
     )
 
     rating = datasets.get_dataset_rating_by_slug(DATASET_SLUG)
 
-    assert isinstance(rating, int)
-    assert rating == 0
+    assert isinstance(rating, str)
+    assert rating == '5.00'
+
+
+@responses.activate
+def test_file_privacy_violation(datasets: Datasets):
+    mock_post_auth()
+    responses.add(
+        responses.POST,
+        join_base_url_values(['privacy-violations']),
+        status=201
+    )
+
+    violation = datasets.file_privacy_violations(PROTO_DATASET_ID,
+                                                 "This is long privacy violation message for testing.")
+
+    assert violation == 'Submitted'
+
+
+@responses.activate
+def test_negative_file_privacy_violation(datasets: Datasets):
+    mock_post_auth()
+    responses.add(
+        responses.POST,
+        join_base_url_values(['privacy-violations']),
+        status=201
+    )
+
+    with pytest.raises(AvaandmedException):
+        datasets.apply_for_access(
+            PROTO_DATASET_ID, "")
+
+    # > 1000 characters
+    with pytest.raises(AvaandmedException):
+        datasets.apply_for_access(PROTO_DATASET_ID, """D0roypVrlJRk8ocBSRtQGXesa4PaZOHDsLH4HDKbUgqkB
+        nMVLgvyXTwoa3BOxGSPnFIfVjJoEEzp75JAe3a65auWTavf3vecdurrjjV9LKGyo48ThzONFa6u0DX7IUoRiIEUkfCAb
+        iqzXfsVa543qD6R3BKTtyDabYdmZfEiahMiQ6GyCt90WeS1CpEmb2pJHHLZ6lQu
+        x8tclXlcXqBXATDGTWzgWc8E23Dk5TnJdT4ZcEDZLAFhieiYvebe98Sq2pK0rkc
+        Ik56MVctve2d1FOmtbypO9iRvgBSOjl1aweuIcpKK8HVnVLVsc3ssGTSYQuyZya
+        zHxfA4xsi9kla09GezkIGllpJUJBblkqAv0hDVNSbAAlmx0TFDhOl6ue2LsoBLP
+        LiIyEdkeFe17VQp2xvKbbguY4DMXjpyQUl6OgcfS5tpyOoXN1aXjBAVTx3sAfYe
+        qo44Y3cM0wHkZIWeixTJ0VS035xs4GQd1CA5tw2Ym5H2sxwR1Q2wNca0XjdA9x7
+        n38TCavreODCq3tVXveNZNl77hGTr1HNp8zdDsGlKsICy3qhBPelFSTSC855NrA
+        ZnnT6OW1HT4QokYKraib5J4FkQ3m6533TJSVKyAb3TisQOi4TsyrSIuhqWtYHhs
+        lSUtZxDd7jLdXCVstmBGDlBXdB8kJQEvYf3tSOvWpvyq3r2YdtytIdfDrXQU9Jj
+        haT3oMpRE8UcecvBBbqxaIZCHBEepZ2ZMXmdZUY4R1vvEyZGn2r388jULxFdl4U
+        XSKIRUh0ch4UNOJDCqBASGEOalB39ezgsKoBlwjRSSuT61Dhjtk4AXBVJk6ogdL
+        EUTpw2Hx7y6OWHIwOj6JNqmOBDiYwDZHCbUaN7MHqAsHuAXE889MdtzmgilbXVk
+        o44RNWMUEwJIDhXnRfSQxqrKioJj3Iv66c8lzEKUdNucCAFDFj71ut7aLxyy3Xf
+        eHhVkPP9z3MjI9ug5f9Q0W8qvLCkSYw4x""")
+
+
+@responses.activate
+def test_apply_for_permissions(datasets: Datasets):
+    mock_post_auth()
+    responses.add(
+        responses.POST,
+        join_base_url_values(['access-permissions']),
+        status=201
+    )
+
+    access = datasets.apply_for_access(PROTO_DATASET_ID,
+                                       "This is long access permissions message for testing.")
+
+    assert access == 'Submitted'
+
+
+@responses.activate
+def test_negative_apply_for_permissions(datasets: Datasets):
+    mock_post_auth()
+    responses.add(
+        responses.POST,
+        join_base_url_values(['access-permissions']),
+        status=201
+    )
+
+    with pytest.raises(AvaandmedException):
+        datasets.apply_for_access(
+            PROTO_DATASET_ID, "")
+
+    # > 1000 characters
+    with pytest.raises(AvaandmedException):
+        datasets.apply_for_access(PROTO_DATASET_ID, """D0roypVrlJRk8ocBSRtQGXesa4PaZOHDsLH4HDKbUgqkB
+        nMVLgvyXTwoa3BOxGSPnFIfVjJoEEzp75JAe3a65auWTavf3vecdurrjjV9LKGyo48ThzONFa6u0DX7IUoRiIEUkfCAb
+        iqzXfsVa543qD6R3BKTtyDabYdmZfEiahMiQ6GyCt90WeS1CpEmb2pJHHLZ6lQu
+        x8tclXlcXqBXATDGTWzgWc8E23Dk5TnJdT4ZcEDZLAFhieiYvebe98Sq2pK0rkc
+        Ik56MVctve2d1FOmtbypO9iRvgBSOjl1aweuIcpKK8HVnVLVsc3ssGTSYQuyZya
+        zHxfA4xsi9kla09GezkIGllpJUJBblkqAv0hDVNSbAAlmx0TFDhOl6ue2LsoBLP
+        LiIyEdkeFe17VQp2xvKbbguY4DMXjpyQUl6OgcfS5tpyOoXN1aXjBAVTx3sAfYe
+        qo44Y3cM0wHkZIWeixTJ0VS035xs4GQd1CA5tw2Ym5H2sxwR1Q2wNca0XjdA9x7
+        n38TCavreODCq3tVXveNZNl77hGTr1HNp8zdDsGlKsICy3qhBPelFSTSC855NrA
+        ZnnT6OW1HT4QokYKraib5J4FkQ3m6533TJSVKyAb3TisQOi4TsyrSIuhqWtYHhs
+        lSUtZxDd7jLdXCVstmBGDlBXdB8kJQEvYf3tSOvWpvyq3r2YdtytIdfDrXQU9Jj
+        haT3oMpRE8UcecvBBbqxaIZCHBEepZ2ZMXmdZUY4R1vvEyZGn2r388jULxFdl4U
+        XSKIRUh0ch4UNOJDCqBASGEOalB39ezgsKoBlwjRSSuT61Dhjtk4AXBVJk6ogdL
+        EUTpw2Hx7y6OWHIwOj6JNqmOBDiYwDZHCbUaN7MHqAsHuAXE889MdtzmgilbXVk
+        o44RNWMUEwJIDhXnRfSQxqrKioJj3Iv66c8lzEKUdNucCAFDFj71ut7aLxyy3Xf
+        eHhVkPP9z3MjI9ug5f9Q0W8qvLCkSYw4x""")
+
+
+@responses.activate
+def test_rate_dataset(datasets: Datasets):
+    mock_post_auth()
+    responses.add(
+        responses.POST,
+        join_base_url_values(['rating']),
+        status=201
+    )
+
+    rating = datasets.rate_dataset(
+        'c114a8a9-40ed-46c9-824f-dda1d92776de', 1, 1)
+
+    assert rating == 'Submitted'
+
+
+@responses.activate
+def test_negative_rate_dataset(datasets: Datasets):
+    mock_post_auth()
+    responses.add(
+        responses.POST,
+        join_base_url_values(['rating']),
+        status=201
+    )
+
+    with pytest.raises(AvaandmedException):
+        rating = datasets.rate_dataset(
+            'c114a8a9-40ed-46c9-824f-dda1d92776de', -1, 11)
+
+    with pytest.raises(AvaandmedException):
+        rating = datasets.rate_dataset(
+            'c114a8a9-40ed-46c9-824f-dda1d92776de', 0, -1)
+
+    with pytest.raises(AvaandmedException):
+        rating = datasets.rate_dataset(
+            'c114a8a9-40ed-46c9-824f-dda1d92776de', 11, -1)
 
 
 # def test_download_file(datasets: Datasets):
