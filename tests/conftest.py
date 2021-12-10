01e9fc42502b809c012758dcbb8faeb2d1502d44
mihhail <mihhail.matisinets@gmail.com>
2021-12-10 12:56:56 +0200

    Refactored tests.
    TL;DR: More reusability, less repetitions.
    
    Made tests to reuse some common objects like mocked data.
    Created RequestMock class to stub HTTP request. This can be reused
    across all test classes thnks to pytest.fixtures feature.
    Moved all mock data into DataJsonMock, made it reusable as well.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import os
 import pytest
 from avaandmed import Avaandmed
+from tests.data_mock import DataJsonMock
+from tests.request_mock import RequestMock
 
 API_TOKEN = os.getenv('AVAANDMED_TEST_KEY', 'none')
 KEY_ID = os.getenv('AVAANDMED_TEST_KEY_ID', 'none')
@@ -20,3 +22,18 @@ def key_id():
 @pytest.fixture
 def avaandmed_client():
     return Avaandmed(API_TOKEN, KEY_ID, BASE_HOSTNAME)
+
+
+@pytest.fixture()
+def datasets(avaandmed_client: Avaandmed):
+    return avaandmed_client.datasets
+
+
+@pytest.fixture(scope='class')
+def request_mock():
+    return RequestMock()
+
+
+@pytest.fixture(scope='class')
+def data_mock():
+    return DataJsonMock()
