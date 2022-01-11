39fc08022bbfadc03439d703d11848dd855899f9
mihhail <mihhail.matisinets@gmail.com>
2022-01-11 12:09:54 +0200

    new constants for mock JSON responses

--- tests/data_mock.py
+++ tests/data_mock.py
@@ -4,16 +4,25 @@ from .utils import load_json
 DATA_DIR = Path('data')
 
 
+def load_data_from(filename: str):
+    return load_json(DATA_DIR / filename)
+
+
 class DataJsonMock:
 
     def __init__(self) -> None:
         # Mock data/files
-        self.MOCK_DATASET_FILE = load_json(DATA_DIR / 'dataset.json')
-        self.MOCK_DATSET_FILE_2 = load_json(DATA_DIR / 'dataset2.json')
-        self.MOCK_ERROR_FILE = load_json(DATA_DIR / 'error.json')
-        self.MOCK_TOKEN_FILE = load_json(DATA_DIR / 'token.json')
-        self.MOCK_DATASET_LIST_FILE = load_json(DATA_DIR / 'dataset_list.json')
-        self.MOCK_PREVIEW_FILE = load_json(DATA_DIR / 'preview.json')
-        self.MOCK_COLUMNS_FILE = load_json(DATA_DIR / 'file_columns.json')
-        self.MOCK_SEARCH_RESULTS = load_json(DATA_DIR / 'search.json')
-        self.MOCK_TOKEN_FILE = load_json(DATA_DIR / 'token.json')
+        self.MOCK_DATASET_FILE = load_data_from('dataset.json')
+        self.MOCK_DATSET_FILE_2 = load_data_from('dataset2.json')
+        self.MOCK_ERROR_FILE = load_data_from('error.json')
+        self.MOCK_TOKEN_FILE = load_data_from('token.json')
+        self.MOCK_DATASET_LIST_FILE = load_data_from('dataset_list.json')
+        self.MOCK_PREVIEW_FILE = load_data_from('preview.json')
+        self.MOCK_COLUMNS_FILE = load_data_from('file_columns.json')
+        self.MOCK_SEARCH_RESULTS = load_data_from('search.json')
+        self.MOCK_TOKEN_FILE = load_data_from('token.json')
+        self.MOCK_PRIVACY_VIOLATIONS = load_data_from(
+            'privacy-violations.json')
+        self.MOCK_ACCESS_PERMISSIONS = load_data_from('access-perms.json')
+        self.MOCK_FILES_LIST = load_data_from('files.json')
+        self.MOCK_FILE_INDEX = load_data_from('file_index.json')
